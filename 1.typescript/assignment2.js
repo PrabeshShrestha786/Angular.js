7f1012cba5c4c0f2bc8ca56f0549d10a5a191e2e
Prabesh786 <e2301430@vamk.fi>
2026-01-22 03:42:12 +0200

    Chapter 1 task 2

---
+++ 1.typescript/assignment2.js
@@ -0,0 +1,6 @@
+function print(name, occupation, wage) {
+    return `Hey, ${name}. You are an ${occupation} and your hourly wage is ${wage} dollars.`;
+}
+
+// Test the function
+console.log(print("Prabesh", "Engineer", 25));
